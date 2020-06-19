9cfe9951afd2079ecdd1eb04fc848b32a6f2b2c8
Shani Levy <shanil@jfrog.com>
2020-06-19 15:09:54 +1000

    Update Hello.vue

--- npm-frontend-service/src/components/Hello.vue
+++ npm-frontend-service/src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <div style="height:7em"></div>
-    <img alt="frog" src="./../assets/SwampupOnlinelogo.png" class="responsive"/>    <div style="height:2em"></div>
+    <img alt="frog" src="./../assets/SwampupOnlinelogoi.png" class="responsive"/>    <div style="height:2em"></div>
     <h1 class="elementor-heading-title elementor-size-default">BECOME A LEAN, MEAN <span style="color:#40be46;&quot;">DEVOPS MACHINE</span></h1>  </div>
 </template>
 
